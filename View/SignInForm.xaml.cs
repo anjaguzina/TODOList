83047545195c8ec79ffcd5b15ff73f72b4750b1c
Anja Guzina <anjaguzina15@gmail.com>
2025-07-08 21:40:51 +0200

    sign in form i user repozitorijum

---
+++ View/SignInForm.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TODOList.Model;
+using TODOList.Repository;
+
+namespace TODOList.View
+{
+    /// <summary>
+    /// Interaction logic for SignInForm.xaml
+    /// </summary>
+    public partial class SignInForm : Window
+    {
+        private readonly UserRepository _repository;
+        public SignInForm()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _repository = new UserRepository();
+        }
+    }
+}
