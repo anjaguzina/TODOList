ac004d370c9e55ac6b310f3e571fe8448b07b046
Anja Guzina <anjaguzina15@gmail.com>
2025-07-08 21:04:14 +0200

    dodat task.cs

--- Model/Task.cs
+++ Model/Task.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using TODOList.Serializer;
 
 namespace TODOList.Model
 {
-    internal class Task
+    public class Task: ISerializable
     {
+        public int Id { get; set; }                     
+        public string Title { get; set; }                
+        public string Description { get; set; }          
+        public DateTime DueDate { get; set; }           
+        public bool IsCompleted { get; set; }
+
+        public Task() { }
+
+        public Task(string title, string description, DateTime duedate, bool isdone) {
+            Title = title;
+            Description = description;
+            DueDate = duedate;
+            IsCompleted = isdone;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Title, Description, DueDate.ToString("dd-MM-yyyy HH:mm:ss"),IsCompleted.ToString() };
+            return csvValues;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Title = values[1];
+            Description = values[2];
+            DueDate = DateTime.ParseExact(values[3], "dd-MM-yyyy HH:mm:ss", null);
+            IsCompleted = bool.Parse(values[4]);
+        }
     }
 }
